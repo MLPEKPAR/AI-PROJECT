228a646b1351388782b00aca2b6a71a804eb7fde
MLPEKPAR <rose.marialuisa@gmail.com>
2025-02-04 12:30:47 +0100

    adding basic JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
+function displayWOD(event) {
+  event.preventDefault();
+  let answerEl = document.querySelector("#answer");
+  answerEl.innerHTML = "el perro de san roque....";
+}
 
+let formEl = document.querySelector("#form");
+formEl.addEventListener("submit", displayWOD);
