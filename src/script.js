3db43ecae8a1ea0641abbc9b9103157e6dd2545b
MLPEKPAR <rose.marialuisa@gmail.com>
2025-02-05 20:04:32 +0100

    Touch-ups
    HTML change of name and title
    CSS    adding blinking text effect, polishing style attributes
    JS      adding typewriter effect on answer text; improving AI prompt
    ; adding  / removing style elements via JS

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 function displayWOD(response) {
   let answerEl = document.querySelector("#answer");
+  answerEl.classList.remove("blink_me");
   answerEl.innerHTML = response.data.answer;
+
+  new Typewriter("#answer", {
+    strings: `${response.data.answer}`,
+    autoStart: true,
+  });
 }
 
 function generateWOD(event) {
@@ -9,10 +15,17 @@ function generateWOD(event) {
   let instructions = document.querySelector("#request");
   let prompt = "Propose a single WOD (workout of the day)";
   let context = `You are an experienced CrossFit athlete and coach. Use your knowledge and the ${instructions.value} 
-  to present a WOD in a concise and schematic manner using basic HTML. include a line break between movements.
-  Important: Do not include detailed movement descriptions. Keep it clean, structured, and easy to read`;
+  to present a WOD in a concise and schematic manner using **only plain HTML** without backticks or tags. include a line break between movements.
+  Important: Do not include detailed movement descriptions. Keep it clean, structured, and easy to read. The response should consist of html elements only.title font size medium`;
   let apiKey = "11edc9a3d0f3o475000at9446642fb9a";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let waitingEl = document.querySelector("#answer");
+  waitingEl.classList.remove("hidden");
+  waitingEl.classList.add("blink_me");
+
+  waitingEl.innerHTML =
+    "<strong>Get ready to sweat 🏋🏽🏋🏽🏋🏽 </br>🦾🦾🦾The WOD is coming!</strong>";
 
   axios.get(apiUrl).then(displayWOD);
 }
