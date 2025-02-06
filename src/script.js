ab68745d1358eca6a6ef99aba1e2ec0ca476c307
MLPEKPAR <rose.marialuisa@gmail.com>
2025-02-06 10:13:03 +0100

    UX improvements
    CSS style changes
    JS prompt reingeneering

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 function displayWOD(response) {
   let answerEl = document.querySelector("#answer");
-  answerEl.classList.remove("blink_me");
   answerEl.innerHTML = response.data.answer;
 
   new Typewriter("#answer", {
@@ -13,19 +12,21 @@ function generateWOD(event) {
   event.preventDefault();
 
   let instructions = document.querySelector("#request");
-  let prompt = "Propose a single WOD (workout of the day)";
+  let prompt = "Generate a WOD (workout of the day)";
   let context = `You are an experienced CrossFit athlete and coach. Use your knowledge and the ${instructions.value} 
-  to present a WOD in a concise and schematic manner using **only plain HTML** without backticks or tags. include a line break between movements.
-  Important: Do not include detailed movement descriptions. Keep it clean, structured, and easy to read. The response should consist of html elements only.title font size medium`;
+  to present a WOD in a concise and schematic manner using **only plain HTML** without backticks or tags
+   include a line break between movements. Make sure title font size is larger than the rest but still readable.
+    Important: Do not include detailed movement descriptions. 
+    Keep it clean, structured, and easy to read. 
+    The response should consist of html elements only.
+    Sign at the bottom as "Your AI coach" in red color`;
   let apiKey = "11edc9a3d0f3o475000at9446642fb9a";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let waitingEl = document.querySelector("#answer");
   waitingEl.classList.remove("hidden");
-  waitingEl.classList.add("blink_me");
 
-  waitingEl.innerHTML =
-    "<strong>Get ready to sweat 🏋🏽🏋🏽🏋🏽 </br>🦾🦾🦾The WOD is coming!</strong>";
+  waitingEl.innerHTML = `<div class="blink-me"><strong>Get ready to sweat 🏋🏽🏋🏽🏋🏽 </br>🦾🦾🦾The WOD is coming!</strong></div>`;
 
   axios.get(apiUrl).then(displayWOD);
 }
